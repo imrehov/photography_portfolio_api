de44f348f2229966eebd0642da5deb7c7d168583
Imre Hovodzak <imre.hovodzak@gmail.com>
2023-02-10 13:57:49 +0100

    gallery

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return render(request, "home.html")
+    context = {
+        "name": "Imre Hovodzak",
+        "subtitle": "photography"
+    }
+    return render(request, "home.html", context)
 
 def about_view(request):
     return render(request, "about.html")
 
 def contact_view(request):
     return render(request, "contact.html")
+
+def gallery_view(request):
+    return render(request, "gallery.html")
