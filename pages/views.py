fcf7c59a145e5dafd082f95b583c53a3ed502b4c
Imre Hovodzak <imre.hovodzak@gmail.com>
2023-02-21 13:36:05 +0100

    update

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
+class HomeView(APIView):
+    permission_classes = [AllowAny]
 
-# Create your views here.
+    def get(self, request):
+        return Response(
+                {
+                "message": "hello world"
+                }
+            )
