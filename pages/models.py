d5df992243c9adc13eb9aa2c0bda0bcc27c5ed19
Imre Hovodzak <imre.hovodzak@gmail.com>
2023-02-21 12:58:23 +0100

    last before react merge

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.core.validators import FileExtensionValidator
+from PIL import Image
 
 # Create your models here.
 
@@ -21,6 +22,20 @@ class About(models.Model):
         validators=[FileExtensionValidator(["jpg", "jpeg", "png"])]
         )
     text = models.TextField(max_length=2000)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # open img and resize
+
+        image_path = self.image.path
+        img = Image.open(image_path)
+        max_size = 600
+
+        if img.size[0] > max_size or img.size[1] > max_size:
+            img.thumbnail((max_size, max_size))
+            img.save(image_path)
+
 
     def __str__(self):
         return self.text[:30]
